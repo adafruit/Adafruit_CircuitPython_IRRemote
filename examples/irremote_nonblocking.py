96403ae3758beae168308783a4d342f0203b1119
Dan Allan <daniel.b.allan@gmail.com>
2021-05-30 11:31:30 -0400

    Fix import order.

--- examples/irremote_nonblocking.py
+++ examples/irremote_nonblocking.py
@@ -3,10 +3,11 @@
 
 # Circuit Playground Express Demo Code
 # Adjust the pulseio 'board.PIN' if using something else
-import pulseio
-import board
-import adafruit_irremote
 import time
+
+import adafruit_irremote
+import board
+import pulseio
 
 pulsein = pulseio.PulseIn(board.REMOTEIN, maxlen=120, idle_state=True)
 decoder = adafruit_irremote.NonblockingGenericDecode(pulsein)
